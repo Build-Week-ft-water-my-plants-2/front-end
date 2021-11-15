3e7429ab4b21ea94ce2ff07259a1001603877e6d
lgallo95 <lgallo2005@gmail.com>
2021-11-15 12:20:16 -0800

    Added buttons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-import logo from "./logo.svg"
 import "./App.css"
+import React, { useState, useEffect } from 'react'
+import { Link, Route, Switch } from 'react-router-dom';
+import About from './components/About'
+import Signup from './components/Signup'
+import Login from './components/Login'
 
 function App() {
-  return <div className="App">We're done</div>
+  
+  
+  return(
+    <div className= "App">
+      <nav> 
+        <div className= "sign-in-buttons">
+          <Link to="/">
+          <button className="home"> Home </button> 
+          </Link>
+          <Link to="/sign-up">
+          <button className="sign-up"> Sign Up</button>
+        </Link>
+        <Link to="/login">
+          <button className="login"> Login </button>
+        </Link> 
+        </div>
+      </nav>
+      <Switch>
+        <Route path = "/">
+          <About/>
+        </Route>
+        <Route path = "/sign-up">
+          <Signup/>
+        </Route>
+        <Route path = "/login">
+          <Login/>
+        </Route>
+      </Switch>
+    </div>
+)
 }
 
 export default App
+
+
