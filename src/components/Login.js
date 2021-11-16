c51f30339b7d477d5796268e0bba394795c8bfcb
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2021-11-16 10:22:11 -0500

    added disable button functionality to login button

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
-import React from "react"
+import React from 'react'
 
 function Login(props) {
-  const { values, submit, update } = props
+  const { values, submit, update, disabled, errors } = props
   const onSubmit = (evt) => {
     evt.preventDefault()
     submit()
@@ -13,37 +13,52 @@ function Login(props) {
   }
 
   return (
-    <form className="login-wrapper" onSubmit={onSubmit}>
-      {/* PLACEHOLDER FOR IMAGES */}
-      <div className="img-container"></div>
-      <div className="background-img"></div>
+    <form className='login-wrapper' onSubmit={onSubmit}>
+      
+      {/* image placeholder */}
+      <div className='img-container'>
+        <img src="#" alt="logo"/>
+      </div>
+      <div className='background-img'>
+        <img src="#" alt="logo"/>
+      </div>
 
-      {/* LOGIN CARD */}
-      <div className="form-wrapper">
+      {/* login card */}
+      <div className='form-wrapper'>
         <h2>LOGIN</h2>
         <label>
           <input
             value={values.username}
             onChange={onChange}
-            placeholder="Username"
-            name="username"
-            type="text"
+            placeholder='Username'
+            name='username'
+            type='text'
           />
         </label>
         <label>
           <input
             value={values.password}
             onChange={onChange}
-            placeholder="Password"
-            name="password"
-            type="password"
+            placeholder='Password'
+            name='password'
+            type='password'
           />
         </label>
-        <div className="account-bot">
+
+        { /** disable button */}
+        <div className='form-group submit'>
+          <button disabled={disabled}>Register</button>
+          <div className='errors'>
+            <div>{errors.username}</div>
+            <div>{errors.password}</div>
+          </div>
+        </div>
+
+        <div className='account-bot'>
           <h2>
             Don't have account yet? <span>REGISTER</span>
           </h2>
-          <button className="submit-login">LOGIN</button>
+          <button className='submit-login'>LOGIN</button>
         </div>
       </div>
     </form>
@@ -51,3 +66,6 @@ function Login(props) {
 }
 
 export default Login
+
+
+//** Styling below */
