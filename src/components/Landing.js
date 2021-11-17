64423df1afb903b34ea4426924f9b80590b6f313
Nelson Maldonado <play2nino@gmail.com>
2021-11-17 13:45:55 -0500

    commented out plant details because it's no in use

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,48 +1,48 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import url from './URL'
-import Plant from './Plant'
-import PlantDetails from './PlantDetails'
-import PlantForm from './PlantForm'
-
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import url from "./URL"
+import Plant from "./Plant"
+// import PlantDetails from './PlantDetails'
+import PlantForm from "./PlantForm"
 
 const initialFormValues = {
-    nickname: '',
-    species: '',
-    h2oFrequency: '',
-    imageURL: '',
+  nickname: "",
+  species: "",
+  h2oFrequency: "",
+  imageURL: "",
 }
 
 const initialPlants = []
 
 function PlantPage() {
-
-  const [plants, setPlants] = useState(initialPlants)          
-  const [formValues, setFormValues] = useState(initialFormValues) 
+  const [plants, setPlants] = useState(initialPlants)
+  const [formValues, setFormValues] = useState(initialFormValues)
 
   const getPlants = () => {
-    axios.get(`${url}/api/plants`)
-      .then(res => {
-        setPlants(res.data);
+    axios
+      .get(`${url}/api/plants`)
+      .then((res) => {
+        setPlants(res.data)
       })
-      .catch(err => console.error(err))
+      .catch((err) => console.error(err))
   }
 
-  const postNewPlant = newPlant => {
-    axios.post(`${url}/api/plants`, newPlant)
-      .then(res => {
-        setPlants([res.data, ...plants]);
+  const postNewPlant = (newPlant) => {
+    axios
+      .post(`${url}/api/plants`, newPlant)
+      .then((res) => {
+        setPlants([res.data, ...plants])
       })
-      .catch(err => console.error(err))
+      .catch((err) => console.error(err))
       .finally(() => {
-        setFormValues(initialFormValues);
+        setFormValues(initialFormValues)
       })
   }
 
   const inputChange = (name, value) => {
     setFormValues({
       ...formValues,
-      [name]: value 
+      [name]: value,
     })
   }
 
@@ -53,34 +53,26 @@ function PlantPage() {
       h2oFrequency: formValues.h2oFrequency.trim(),
       imageURL: formValues.imageURL,
     }
-    postNewPlant(newPlant);
-}
+    postNewPlant(newPlant)
+  }
 
-    useEffect(() => {
-        getPlants()
-    }, [])
+  useEffect(() => {
+    getPlants()
+  }, [])
 
-  return(
-    <div className='container'>
-      <header><h1>Plant Database</h1></header>
+  return (
+    <div className="container">
+      <header>
+        <h1>Plant Database</h1>
+      </header>
 
-      <PlantForm
-        values={formValues}
-        change={inputChange}
-        submit={formSubmit}
-      />
+      <PlantForm values={formValues} change={inputChange} submit={formSubmit} />
 
-      {
-        plants.map(plant => {
-          return (
-            <Plant key={plant.id} details={plant} />
-          )
-        })
-      }
-      </div>
+      {plants.map((plant) => {
+        return <Plant key={plant.id} details={plant} />
+      })}
+    </div>
   )
 }
 
-
-
-export default PlantPage;
+export default PlantPage
