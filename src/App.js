fda57691b57e12cf3a761075b876c436617baa03
Tim Marchant <fromtimwithcode@gmail.com>
2021-11-17 14:30:37 -0600

    appbar

--- src/App.js
+++ src/App.js
@@ -3,31 +3,45 @@ import { Link, Route, Switch } from "react-router-dom"
 import About from "./components/About"
 import Signup from "./components/Signup"
 import Login from "./components/Login"
-import styled from "styled-components"
 
-const Bttn = styled.div`
-  display: flex;
-  flex-direction: row-reverse;
-`
+import Box from "@mui/material/Box"
+import AppBar from "./components/AppBar"
+import Toolbar from "./components/Toolbar"
 
 function App() {
   return (
-    <div className="App">
-      <nav>
-        <div className="buttons">
-          <Bttn>
-            <Link to="/">
-              <button className="home"> Home </button>
+    <div>
+      <AppBar position="fixed">
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          <Box sx={{ flex: 1 }} />
+          <Link
+            to="/"
+            style={{ fontSize: 24, textDecoration: "none", color: "inherit" }}
+          >
+            {"Water My Plants"}
+          </Link>
+          <Box sx={{ flex: 1, display: "flex", justifyContent: "flex-end" }}>
+            <Link
+              to="/login"
+              style={{
+                fontSize: 18,
+                textDecoration: "none",
+                color: "inherit",
+                marginRight: "1rem",
+              }}
+            >
+              {"Login"}
             </Link>
-            <Link to="/sign-up">
-              <button className="sign-up"> Sign Up</button>
+            <Link
+              to="/sign-up"
+              style={{ fontSize: 18, textDecoration: "none", color: "inherit" }}
+            >
+              {"Sign Up"}
             </Link>
-            <Link to="/login">
-              <button className="login"> Login </button>
-            </Link>
-          </Bttn>
-        </div>
-      </nav>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
       <Switch>
         <Route exact path="/">
           <About />
